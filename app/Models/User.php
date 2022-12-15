b865b591b417296d29f13d34252d0863fe5de5b1
yogaiw <agoy.indra@gmail.com>
2022-12-15 15:49:37 +0700

    preparing migration for user table

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'id_by_campus',
         'email',
         'password',
+        'role'
     ];
 
     /**
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('id_by_campus')->unique();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('student');
             $table->rememberToken();
             $table->timestamps();
         });
