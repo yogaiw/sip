dd4a1d2f8f7f2753a37b7ab9d902c3bd28ab2b8c
yogaiw <agoy.indra@gmail.com>
2022-12-30 04:04:13 +0700

    add clockwork dependency to check performance

--- resources/views/student/main.blade.php
+++ resources/views/student/main.blade.php
@@ -301,13 +301,6 @@
     <!-- Custom scripts for all pages-->
     <script src="sb-admin-assets/js/sb-admin-2.min.js"></script>
 
-    <!-- Page level plugins -->
-    <script src="sb-admin-assets/vendor/chart.js/Chart.min.js"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="sb-admin-assets/js/demo/chart-area-demo.js"></script>
-    <script src="sb-admin-assets/js/demo/chart-pie-demo.js"></script>
-
 </body>
 
 </html>
